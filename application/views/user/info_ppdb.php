7c9aaa7d3aece79373a5cf7ec0cd8bd4e1496a86
fristkhfdz22 <khafidzrizziqalhuda@gmail.com>
2024-10-29 11:37:20 -0700

    done

--- application/views/user/info_ppdb.php
+++ application/views/user/info_ppdb.php
@@ -11,41 +11,6 @@
         <h1>Informasi PPDB</h1>
     </header>
     
-    <nav>
-        <ul>
-            <li><a href="<?= base_url('user/home'); ?>">Home</a></li>
-            <li><a href="<?= base_url('user/info_ppdb'); ?>">Info PPDB</a></li>
-            <!-- Tambahkan menu lainnya sesuai kebutuhan -->
-        </ul>
-    </nav>
-
-    <main>
-        <?php if (!empty($info_ppdb)): ?>
-            <?php foreach ($info_ppdb as $item): ?>
-                <article>
-                    <h2><?= $item->title; ?></h2>
-                    <img src="<?= base_url('uploads/' . $item->image); ?>" alt="<?= $item->title; ?>" style="max-width: 100%;">
-                    <p><?= $item->description; ?></p>
-                    <div class="social-links">
-                        <a href="<?= $item->youtube; ?>" target="_blank">YouTube</a>
-                        <a href="<?= $item->instagram; ?>" target="_blank">Instagram</a>
-                        <a href="<?= $item->facebook; ?>" target="_blank">Facebook</a>
-                        <a href="<?= $item->twitter; ?>" target="_blank">Twitter</a>
-                        <a href="<?= $item->telegram; ?>" target="_blank">Telegram</a>
-                    </div>
-                    <hr>
-                </article>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <p>Tidak ada informasi PPDB yang tersedia saat ini.</p>
-        <?php endif; ?>
-    </main>
-
-<footer>
-
-</footer>
-</body>
-</html>
     <br>
     <br>
     <br>
@@ -142,3 +107,10 @@
             width: 20px;
         }
     </style>
+
+<footer>
+
+</footer>
+</body>
+</html>
+   
